8f4046ad70a7e8cc299d71eeb89f81b6724dcd09
Kim Chang Dae <windowbig@gmail.com>
2019-11-03 23:33:18 +0900

    Red - 5CHF * 2 = 10CHF
    테스트케이스 작성

--- tddDollarExampleSwiftTests/tddDollarExampleSwiftTests.swift
+++ tddDollarExampleSwiftTests/tddDollarExampleSwiftTests.swift
@@ -11,15 +11,16 @@ import XCTest
 
 // MARK: - Todo List
 /*
- $5 + 10CHF = $10(환율이 2:1일 경우)
- $5 * 2 = $10
- amount를 private로 만들기
- Dollar side effect
- Money 반올림
- equals()
- hashCode()
- Equal nil
- Equal object
+ [] $5 + 10CHF = $10(환율이 2:1일 경우)
+ [v] $5 * 2 = $10
+ [v] amount를 private로 만들기
+ [v] Dollar side effect
+ [] Money 반올림
+ [v] equals()
+ [] hashCode()
+ [] Equal nil
+ [] Equal object
+ [] 5CHF * 2 = 10CHF
  */
 
 class tddDollarExampleSwiftTests: XCTestCase {
@@ -33,5 +34,11 @@ class tddDollarExampleSwiftTests: XCTestCase {
   func testEquality() {
     XCTAssertTrue(Dollar(5) == Dollar(5))
     XCTAssertFalse(Dollar(5) == Dollar(6))
+  }
+  
+  func testFrancMultiplication() {
+    let five = Franc(5)
+    XCTAssertEqual(Franc(10), five.times(2))
+    XCTAssertEqual(Franc(15), five.times(3))
   }
 }
